2d40d69fd5bc0534c3d21b875461c48194a88a33
NguyenLoi851 <nguyensyloi851@gmail.com>
2022-05-06 10:52:59 +0700

    format contract code

--- smart-contract/contracts/Campaign.sol
+++ smart-contract/contracts/Campaign.sol
@@ -3,12 +3,13 @@ pragma solidity ^0.8.0;
 
 contract CampaignFactory {
     Campaign[] public deployedCampaigns;
+
     function createCampaign(uint256 minimum) public {
         Campaign newCampaign = new Campaign(minimum, msg.sender);
         deployedCampaigns.push(newCampaign);
-    } 
+    }
 
-    function getDeployedCampaigns() public view returns(Campaign[] memory) {
+    function getDeployedCampaigns() public view returns (Campaign[] memory) {
         return deployedCampaigns;
     }
 }
@@ -37,12 +38,19 @@ contract Campaign {
     }
 
     function contribute() public payable {
-        require(msg.value >= minimumContribution, "A minimum contribution is required");
+        require(
+            msg.value >= minimumContribution,
+            "A minimum contribution is required"
+        );
         approvers[msg.sender] = true;
         approversCount++;
     }
 
-    function createRequest(string memory description, uint256 value, address payable recipient) public onlyManager {
+    function createRequest(
+        string memory description,
+        uint256 value,
+        address payable recipient
+    ) public onlyManager {
         Request storage r = requests[numRequests++];
         r.description = description;
         r.value = value;
@@ -53,22 +61,50 @@ contract Campaign {
 
     function approveRequest(uint256 index) public {
         Request storage request = requests[index];
-        require(approvers[msg.sender], "Only contributors can approve a specific payment request");
-        require(!request.approvals[msg.sender], "You have already voted to approve this request");
+        require(
+            approvers[msg.sender],
+            "Only contributors can approve a specific payment request"
+        );
+        require(
+            !request.approvals[msg.sender],
+            "You have already voted to approve this request"
+        );
         request.approvals[msg.sender] = true;
         request.approvalCount++;
     }
 
     function finalizeRequest(uint256 index) public onlyManager {
         Request storage request = requests[index];
-        require(request.approvalCount > (approversCount / 2), "This request needs more approvals before it can be finalized");
-        require(request.complete == false, "This request has already been finalized");
+        require(
+            request.approvalCount > (approversCount / 2),
+            "This request needs more approvals before it can be finalized"
+        );
+        require(
+            request.complete == false,
+            "This request has already been finalized"
+        );
         request.recipient.transfer(request.value);
         request.complete = true;
     }
 
-    function getSummary() public view returns(uint256, uint256, uint256, uint256, address) {
-        return (minimumContribution, address(this).balance, numRequests, approversCount, manager);
+    function getSummary()
+        public
+        view
+        returns (
+            uint256,
+            uint256,
+            uint256,
+            uint256,
+            address
+        )
+    {
+        return (
+            minimumContribution,
+            address(this).balance,
+            numRequests,
+            approversCount,
+            manager
+        );
     }
 
     function getRequestsCount() public view returns (uint256) {
@@ -76,7 +112,10 @@ contract Campaign {
     }
 
     modifier onlyManager() {
-        require(msg.sender == manager, "Only the campaign manager can call this function");
+        require(
+            msg.sender == manager,
+            "Only the campaign manager can call this function"
+        );
         _;
     }
 }
