ce4a07701d8d7879430db0267aa27b7c3ff4261b
NguyenLoi851 <nguyensyloi851@gmail.com>
2022-05-07 23:55:32 +0700

    update deploy.js

--- smart-contract/scripts/deploy.js
+++ smart-contract/scripts/deploy.js
@@ -1,27 +1,27 @@
 const fs = require("fs");
 const hre = require("hardhat");
 
+const campaign = require("../artifacts/contracts/Campaign.sol/Campaign.json");
+
 const main = async () => {
 
   const CampaignFactory = await hre.ethers.getContractFactory("CampaignFactory");
   const campaignFactory = await CampaignFactory.deploy();
 
-  // const Campaign = await hre.ethers.getContractFactory("Campaign");
-  // const campaign = await Campaign.deploy();
-
   await campaignFactory.deployed();
 
-  // await campaign.deployed();
-
   console.log("CampaignFactory deployed to:", campaignFactory.address);
-
-  // console.log("Campaign deployed to:", campaign.address);
 
   const data = {
     address: campaignFactory.address,
     abi: JSON.parse(campaignFactory.interface.format('json'))
   }
   fs.writeFileSync('../client/src/utils/CampaignFactory.json', JSON.stringify(data));
+  const campaignAbi = {
+    abi: campaign.abi,
+  };
+  fs.writeFileSync("../client/src/utils/Campaign.json", JSON.stringify(campaignAbi));
+
 }
 
 const runMain = async () => {
